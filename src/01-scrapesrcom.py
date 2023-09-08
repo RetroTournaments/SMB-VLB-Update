4aea9533364b363ead06a7387e4ee4f7b0bdecea
Matthew <matthew@retrotournaments.org>
2023-09-08 13:51:04 -0600

    update script, and run it to generate 01-runs.csv

--- src/01-scrapesrcom.py
+++ src/01-scrapesrcom.py
@@ -1,13 +1,14 @@
 # This script is the first script.
-# It uses srcomapi (https://github.com/blha303/srcomapi) to scrape speedrun.com and identify all of the runs that still need fm2 files.
+# It uses srcomapi (https://github.com/blha303/srcomapi) to scrape speedrun.com and identify all of the runs that must be recreated
 # To install srcomapi follow the directions at the above link.
+
+# This creates (with some user interaction..) a csv (01-runs.csv) that lists all of the runs and relevant information for the next step.
 
 import srcomapi
 import srcomapi.datatypes as dt
-from pathlib import Path
 
 TIME_CUTOFF_MS = 300000
-FM2_DIRECTORY = "../fm2/"
+OUTPUT = "01-runs.csv"
 
 api = srcomapi.SpeedrunCom()
 smb = api.search(srcomapi.datatypes.Game, {"name": "super mario bros."})[0]
@@ -16,31 +17,67 @@ anyp = smb.categories[0]
 lb = dt.Leaderboard(api, 
         data=api.get("leaderboards/{}/category/{}?embed=variables".format(smb.id, anyp.id)))
 
-already_done = set()
-fm2_dir = Path(FM2_DIRECTORY)
-for f in fm2_dir.glob("*.fm2"):
-    assert(len(f.stem) == 8)
-    already_done.add(f.stem)
+lines = []
+seen = set()
 
-under_time_cutoff = 0
-todo = 0
-done = 0
+with open(OUTPUT, "r") as f:
+    f.readline()
+    for line in f.readlines():
+        lines.append(line)
+        seen.add(line.split(",")[0])
 
-for r in lb.runs:
-    srcrun = r["run"]
-    srcid = srcrun.id
-    elapsed_ms = srcrun.times["primary_t"] * 1000
 
-    if elapsed_ms <= TIME_CUTOFF_MS:
-        under_time_cutoff += 1
 
-        if srcid not in already_done:
-            todo += 1
-        else:
-            done += 1
+with open (OUTPUT, "w") as f:
+    f.write("src_run_id,src_platform_id,src_region_id,display_name,date_played,elapsed_ms,video_url\n")
+    for line in lines:
+        f.write(line)
+        f.write("\n")
 
-print("under_time_cutoff: ", under_time_cutoff)
-print("todo: ", todo)
-print("done: ", done)
+    for r in lb.runs:
+        srcrun = r["run"]
+        src_run_id = srcrun.id
+        elapsed_ms = srcrun.times["primary_t"] * 1000
 
+        if src_run_id in seen:
+            continue
+
+        if elapsed_ms > TIME_CUTOFF_MS:
+            continue
+
+        src_platform_id = srcrun.system["platform"]
+        src_region_id = srcrun.system["region"]
+        if src_region_id is None:
+            src_region_id = "pr184lqn" # this is a hold-over from 2021 that I don't remember why.
+
+
+        display_name = srcrun.players[0].name
+        date_played = srcrun.date
+
+        url = None
+        if srcrun.videos is not None:
+            if "links" in srcrun.videos:
+                urls = []
+                for l in srcrun.videos["links"]:
+                    urls.append(l["uri"])
+
+                if len(urls) == 1:
+                    url = urls[0]
+                else:
+                    print(f"CHOOSE BEST VIDEO: www.speedrun.com/smb1/run/{src_run_id}")
+                    for i, turl in enumerate(urls):
+                        print(i, turl)
+
+                    l = input("index: ")
+                    url = urls[int(l)]
+
+        if url is not None:
+            while url.find('&') != -1:
+                print(url)
+                print("WARNING URL CONTAINS AN '&', enter updated url")
+                url = input("url: ")
+
+
+        print(f"{src_run_id},{src_platform_id},{src_region_id},{display_name},{date_played},{elapsed_ms},{url}\n")
+        f.write(f"{src_run_id},{src_platform_id},{src_region_id},{display_name},{date_played},{elapsed_ms},{url}\n")
 
