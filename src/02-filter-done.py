c6a8068b323cec6a83a6ffd68d5d1279eff09b25
Matthew <matthew@retrotournaments.org>
2023-09-08 13:51:32 -0600

    add 02-filter-done.py to remove runs that already have TASes

---
+++ src/02-filter-done.py
@@ -0,0 +1,26 @@
+# This script is the second script.
+# Because some of the runs are already done (their fm2 files are already in the repo) the timestamps and video information do not need to be defined.
+# This filters 01-runs.csv and creates 02-to-crop.csv, which goes to the google sheet and discord for manual verification, time stamp definition, and crop definition somehow..
+
+INPUT = "01-runs.csv"
+OUTPUT = "02-to-crop.csv"
+FM2_DIRECTORY = "../fm2/"
+
+from pathlib import Path
+
+seen = set()
+
+fm2_dir = Path(FM2_DIRECTORY)
+for f in fm2_dir.glob("*.fm2"):
+    assert(len(f.stem) == 8)
+    seen.add(f.stem)
+
+with open(OUTPUT, "w") as fout:
+    with open(INPUT, "r") as fin:
+        fout.write(fin.readline())
+
+        for line in fin.readlines():
+            srcid = line.split(",")[0]
+            if srcid not in seen:
+                fout.write(line)
+
