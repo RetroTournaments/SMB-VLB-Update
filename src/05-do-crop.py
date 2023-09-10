2d20218e088b1f5c75ff490c6e9108f47a21a1f8
Matthew <matthew@retrotournaments.org>
2023-09-09 19:33:38 -0600

    add size_runs.py which helps do-crop, do-perspective, and do-uncrt

--- src/05-do-crop.py
+++ src/05-do-crop.py
@@ -3,70 +3,40 @@
 # Uses ffmpeg.
 
 import subprocess
-from pathlib import Path
+from size_runs import *
 
-CROP = "04-crop.csv"
-TS = "05-timestamps.csv"
-RAW_DIRECTORY = "../raw/"
-VID_DIRECTORY = "../vid/"
+def crop_vid(in_path, out_path, crop, timestamp_start, timestamp_end):
+    vs = crop.split("_")
+    cx, cy, cw, ch = [int(round(float(v))) for v in vs[3:7]]
 
-seen = set()
+    px = 0
+    py = 0
+    pw = 'iw'
+    ph = 'ij'
 
-raw_dir = Path(RAW_DIRECTORY)
+    if cx < 0:
+        pw = f'max(iw + {-cx}\\, {cw})'
+        px = -cx
+        cx = 0
+    else:
+        pw = f'max(iw\\, {cx + cw})'
 
-vid_dir = Path(VID_DIRECTORY)
-vid_dir.mkdir(parents=True, exist_ok=True)
-for f in vid_dir.glob("*"):
-    src_run_id = f.stem
-    assert(len(f.stem) == 8)
-    seen.add(f.stem)
+    if cy < 0:
+        ph = f'max(ih + {-cy}\\, {ch})'
+        py = -cy
+        cy = 0
+    else:
+        ph = f'max(ih\\, {cy + ch})'
 
-timestamps = {}
-with open(TS, "r") as f:
-    f.readline()
-    for line in f.readlines():
-        src_run_id, start, end = line.strip().split(",")
-        timestamps[src_run_id] = (start, end)
+    pad = f'pad={pw}:{ph}:{px}:{py}'
+    crop = f'crop={cw}:{ch}:{cx}:{cy}'
 
+    vf = f"{pad},{crop},scale=256:240:flags=area,setsar=1:1"
 
-with open(CROP, "r") as f:
-    f.readline()
+    args = ['ffmpeg', '-i', vp, '-ss', timestamp_start, '-to', timestamp_end, "-vf", vf, "-vcodec", "libx264", "-crf", "12", ]
 
-    for line in f.readlines():
-        src_run_id, crop = line.strip().split(",")
-        if src_run_id in seen:
-            continue
-        if not crop.startswith("crop"):
-            continue
+    print(" ".join(args))
+    subprocess.call(args)
 
-        vs = crop.split("_")
-        cx, cy, cw, ch = [int(round(float(v))) for v in vs[3:7]]
+process_crop("crop", crop_vid)
 
-        px = 0
-        py = 0
-        pw = 'iw'
-        ph = 'ij'
-
-        if cx < 0:
-            pw = f'max(iw + {-cx}\\, {cw})'
-            px = -cx
-            cx = 0
-        else:
-            pw = f'max(iw\\, {cx + cw})'
-
-        if cy < 0:
-            ph = f'max(ih + {-cy}\\, {ch})'
-            py = -cy
-            cy = 0
-        else:
-            ph = f'max(ih\\, {cy + ch})'
-
-        pad = f'pad={pw}:{ph}:{px}:{py}'
-        crop = f'crop={cw}:{ch}:{cx}:{cy}'
-
-        vf = f"{pad},{crop},scale=256:240:flags=area,setsar=1:1"
-
-        timestamp_start, timestamp_end = timestamps[src_run_id]
-
-        vp = list(raw_dir.glob(f"{src_run_id}*"))[0]
-        subprocess.call(['ffmpeg', '-i', vp, '-ss', timestamp_start, '-to', timestamp_end, "-vf", vf, "-vcodec", "libx264", "-crf", "12", f"{VID_DIRECTORY}{src_run_id}.mp4"])
---
+++ src/size_runs.py
@@ -0,0 +1,55 @@
+# this small file provides helpers for do-crop, do-perspective, and do-uncrt
+
+from pathlib import Path
+
+TS = "05-timestamps.csv"
+RAW_DIRECTORY = "../raw/"
+VID_DIRECTORY = "../vid/"
+CROP = "04-crop.csv"
+
+def get_seen():
+    seen = set()
+
+    raw_dir = Path(RAW_DIRECTORY)
+
+    vid_dir = Path(VID_DIRECTORY)
+    vid_dir.mkdir(parents=True, exist_ok=True)
+    for f in vid_dir.glob("*"):
+        src_run_id = f.stem
+        assert(len(f.stem) == 8)
+        seen.add(f.stem)
+
+    return seen
+
+def get_timestamps():
+    timestamps = {}
+    with open(TS, "r") as f:
+        f.readline()
+        for line in f.readlines():
+            src_run_id, start, end = line.strip().split(",")
+            timestamps[src_run_id] = (start, end)
+
+    return timestamps
+
+def process_crop(prefix, func):
+    seen = get_seen()
+    timestamps = get_timestamps()
+
+    raw_dir = Path(RAW_DIRECTORY)
+
+    with open(CROP, "r") as f:
+        f.readline()
+
+        for line in f.readlines():
+            src_run_id, crop = line.strip().split(",")
+            if src_run_id in seen:
+                continue
+            if not crop.startswith(prefix):
+                continue
+
+            in_path = list(raw_dir.glob(f"{src_run_id}*"))[0]
+            out_path = f"{VID_DIRECTORY}{src_run_id}.mp4"
+
+            timestamp_start, timestamp_end = timestamps[src_run_id]
+
+            func(in_path, out_path, crop, timestamp_start, timestamp_end)
